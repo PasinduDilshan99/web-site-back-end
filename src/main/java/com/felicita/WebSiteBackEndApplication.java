6d72f73b5fb2d79f0a015800e9712f6242a81f6a
PasinduDilshan99 <pd.dimbulana@gmail.com>
2025-09-15 19:27:45 +0530

    initial commit

---
+++ src/main/java/com/felicita/WebSiteBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.felicita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSiteBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSiteBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felicita/WebSiteBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felicita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSiteBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
